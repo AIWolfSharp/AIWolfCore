997d597eab8fba22ffaae0be67d1239cc68da2cd
otsuki <otsuki.yamagata.university@gmail.com>
2016-09-16 12:02:26 +0900

    Delete enumeration type Team.

--- AIWolfLib/Role.cs
+++ AIWolfLib/Role.cs
@@ -62,45 +62,18 @@ namespace AIWolf.Lib
     /// </summary>
     public static class RoleExtensions
     {
-        static Dictionary<Role, Team> roleTeamMap = new Dictionary<Role, Team>();
         static Dictionary<Role, Species> roleSpeciesMap = new Dictionary<Role, Species>();
 
         static RoleExtensions()
         {
-            roleTeamMap[Role.UNC] = Team.UNC;
             roleSpeciesMap[Role.UNC] = Species.UNC;
-
-            roleTeamMap[Role.BODYGUARD] = Team.VILLAGER;
             roleSpeciesMap[Role.BODYGUARD] = Species.HUMAN;
-
-            // This is not used for now.
-            roleTeamMap[Role.FREEMASON] = Team.VILLAGER;
-            roleSpeciesMap[Role.FREEMASON] = Species.HUMAN;
-
-            roleTeamMap[Role.MEDIUM] = Team.VILLAGER;
+            roleSpeciesMap[Role.FREEMASON] = Species.HUMAN; // This is not used for now.
             roleSpeciesMap[Role.MEDIUM] = Species.HUMAN;
-
-            roleTeamMap[Role.POSSESSED] = Team.WEREWOLF;
             roleSpeciesMap[Role.POSSESSED] = Species.HUMAN;
-
-            roleTeamMap[Role.SEER] = Team.VILLAGER;
             roleSpeciesMap[Role.SEER] = Species.HUMAN;
-
-            roleTeamMap[Role.VILLAGER] = Team.VILLAGER;
             roleSpeciesMap[Role.VILLAGER] = Species.HUMAN;
-
-            roleTeamMap[Role.WEREWOLF] = Team.WEREWOLF;
             roleSpeciesMap[Role.WEREWOLF] = Species.WEREWOLF;
-        }
-
-        /// <summary>
-        /// Returns the team the role belongs to.
-        /// </summary>
-        /// <param name="role">Role.</param>
-        /// <returns>The team the role belongs to.</returns>
-        public static Team GetTeam(this Role role)
-        {
-            return roleTeamMap[role];
         }
 
         /// <summary>
