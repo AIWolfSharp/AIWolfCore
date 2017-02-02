2a75b3a1c8874ebc79f34399696b98e35cbde6ac
otsuki <otsuki.yamagata.university@gmail.com>
2017-02-02 14:00:12 +0900

    Merge AIWolfLibException.cs into Error.cs:

--- AIWolfLibShared/Error.cs
+++ AIWolfLibShared/Error.cs
@@ -67,4 +67,36 @@ namespace AIWolf.Lib
             throw new TimeoutException(message);
         }
     }
+
+    /// <summary>
+    /// Exception that occurred in AIWolf library.
+    /// </summary>
+    class AIWolfLibException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of this class.
+        /// </summary>
+        public AIWolfLibException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of this class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        public AIWolfLibException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of this class with a specified error message
+        /// and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception,
+        /// or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public AIWolfLibException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
 }
