94ed5d0cdbe4914e584a2d0f7a2ae63e78f272e0
otsuki <otsuki.yamagata.university@gmail.com>
2017-02-02 13:54:14 +0900

    Move ShuffleExtensions AIWolf.Sample to AIWolf.Lib.

---
+++ AIWolfLibShared/ShuffleExtensions.cs
@@ -0,0 +1,47 @@
+﻿//
+// ShuffleExtensions.cs
+//
+// Copyright (c) 2017 Takashi OTSUKI
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AIWolf.Lib
+{
+#if JHELP
+    /// <summary>
+    /// IEnumerableインターフェースを実装したクラスに対するShuffle拡張メソッド定義
+    /// </summary>
+#else
+    /// <summary>
+    /// Defines extension method to shuffle what implements IEnumerable interface.
+    /// </summary>
+#endif
+    public static class ShuffleExtensions
+    {
+#if JHELP
+        /// <summary>
+        /// IEnumerableをシャッフルしたものを返す
+        /// </summary>
+        /// <typeparam name="T">IEnumerableの要素の型</typeparam>
+        /// <param name="s">TのIEnumerable</param>
+        /// <returns>シャッフルされたIEnumerable</returns>
+#else
+        /// <summary>
+        /// Returns shuffled IEnumerable of T.
+        /// </summary>
+        /// <typeparam name="T">Type of element of IEnumerable.</typeparam>
+        /// <param name="s">IEnumerable of T.</param>
+        /// <returns>Shuffled IEnumerable of T.</returns>
+#endif
+        public static IOrderedEnumerable<T> Shuffle<T>(this IEnumerable<T> s)
+        {
+            return s.OrderBy(x => Guid.NewGuid());
+        }
+    }
+}
--- AIWolfPlayerShared/BasePlayer.cs
+++ AIWolfPlayerShared/BasePlayer.cs
@@ -8,7 +8,6 @@
 //
 
 using AIWolf.Lib;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -382,38 +381,6 @@ namespace AIWolf.Player.Sample
         protected bool IsCo(Role role)
         {
             return comingoutMap.ContainsValue(role);
-        }
-    }
-
-#if JHELP
-    /// <summary>
-    /// IEnumerableインターフェースを実装したクラスに対するShuffle拡張メソッド定義
-    /// </summary>
-#else
-    /// <summary>
-    /// Defines extension method to shuffle what implements IEnumerable interface.
-    /// </summary>
-#endif
-    public static class ShuffleExtensions
-    {
-#if JHELP
-        /// <summary>
-        /// IEnumerableをシャッフルしたものを返す
-        /// </summary>
-        /// <typeparam name="T">IEnumerableの要素の型</typeparam>
-        /// <param name="s">TのIEnumerable</param>
-        /// <returns>シャッフルされたIEnumerable</returns>
-#else
-        /// <summary>
-        /// Returns shuffled IEnumerable of T.
-        /// </summary>
-        /// <typeparam name="T">Type of element of IEnumerable.</typeparam>
-        /// <param name="s">IEnumerable of T.</param>
-        /// <returns>Shuffled IEnumerable of T.</returns>
-#endif
-        public static IOrderedEnumerable<T> Shuffle<T>(this IEnumerable<T> s)
-        {
-            return s.OrderBy(x => Guid.NewGuid());
         }
     }
 }
